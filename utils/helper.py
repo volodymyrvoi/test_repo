ae0a565eb29a616990dce50542507e34c6655d12
volodymyrvo <volodymyrvo@softwareplanetgroup.com>
2025-04-10 12:25:25 +0300

    doStuff

--- utils/helper.py
+++ utils/helper.py
@@ -48,3 +48,17 @@ def make_plot(result, name, fill_between=True):
 
 def do(a, b):
     return sum(1 for i in range(len(a)) if a[i] == b[i])
+
+
+def doStuff(x,y,z=None):
+ a=0
+ while a<10:
+  if x>5:
+   x=x-1
+  elif y<3:
+   y+=2
+  else:
+   print("hi")
+   break
+ return x+y+z
+
